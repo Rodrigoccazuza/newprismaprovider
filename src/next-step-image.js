5f2a6af20a2942a90cd3e625be5ddefff24e8d74
Rodrigo Cazuza <147950045+Rodrigoccazuza@users.noreply.github.com>
2026-08-31 14:52:34 -0400

    Use new editorial image for Your Next Step

---
+++ src/next-step-image.js
@@ -0,0 +1,5 @@
+const finalCtaImage = document.querySelector('.final-cta > img')
+
+if (finalCtaImage) {
+  finalCtaImage.src = `${import.meta.env.BASE_URL}images/editorial/C951D67C-AD2D-4933-A670-A007827FC3A5.png`
+}
